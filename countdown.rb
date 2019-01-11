1dd3241c3aed8014cbb220d7b6bccd802f357372
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-11 18:20:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,17 @@
 #write your code here
 
 def countdown(number)
-  
+  while number > 0
   
   puts "#{number} SECOND(S)!"
+  number -= 1 
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(number)
   
+  
+   puts "#{number} SECOND(S)!"
+  "HAPPY NEW YEAR!"
+end
   
